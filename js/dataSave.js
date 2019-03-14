756bcf5ec16f04f5befc2a6f440bc0259343bc8b
vladbelmas <vladbelmas007@gmail.com>
2019-03-14 11:32:28 +0200

    fix bug with image

--- js/dataSave.js
+++ js/dataSave.js
@@ -22,7 +22,7 @@ function createImage(){
             }
         }
     }
-    request.open('GET', '../img/test.png', true);
+    request.open('GET', './img/test.png', true);
     request.onload = responseload;
     request.send();
 
