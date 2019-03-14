28757baa3e6677c88f790a59df4d03820d6164e5
vladbelmas <vladbelmas007@gmail.com>
2019-03-14 16:41:03 +0200

    14.03 work

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('css/**/*.scss')
+    .pipe(concat('all.scss'))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    //.pipe(cssmin())         // if you want to minify css  uncomment this
+    .pipe(gulp.dest('./dist/'))
 });
 gulp.task('webserver', function() {
     gulp.src('/')
